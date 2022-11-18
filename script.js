d11defa505e3a0c2def10e2352d379763403ec7a
Benjamin Rude <benjaminwrude@gmail.com>
2022-11-17 16:23:12 -0800

    updated Java with first loop. Added picture for second

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function funAnswer(){
 alert("Welcome to my gum emporium");
-let funAnswer = prompt("Are you here to grade my Java? YES or NO or OTHER please");
+let funAnswer = prompt("Are you here to grade my Javascript? YES or NO or OTHER please");
 
-if(funAnswer == ""){
-    alert("It is a simple question. Please answer it")
-    funAnswer = prompt("Are you here to grade my Java? YES or NO or OTHER please");
+while(funAnswer == ""){
+    funAnswer = prompt("Are you here to grade my Javascript? YES or NO");
 }
 if(funAnswer == "YES"){
     alert("I would like the max grade please kind kind TA or Teacher.");
@@ -14,14 +13,37 @@ else if (funAnswer == "NO"){
     alert("Hope you like the color pink...because of Gum")
     document.write("I told you...It is a lot of pink.");
 } 
-else if (funAnswer == "OTHER"){
-    alert("You won the the prize for coming to my gum website! Search the site for your prize!");
-} 
+
+return funAnswer;
+}
+
+function swallowGum(){
+
+    let gum = prompt ("How many pieces of gum do you swallow between 1 and 3?")
+    for (let i = 0; i < 4; i++);{
+        document.write("<img src")
+    }
+}
+
+
+
+
+
+
+
+
+function treasureHunt(){
+     let treasureHunt = Prompt("Thanks for Before you explore the website would you like to find treasure? YES or NO please!");
+
+if(treasureHunt == "YES"){
+    alert("Well go on and find it!")
+    document.write("You have found my hidden gum treasure. Thank you for coming to the bottom of my website!")
+}     
+else if(treasureHunt == "NO"){
+    alert("Wow you are no fun!")
+}
 else{
-    alert("It is a simple yes or no question. Are you kidding me!?");
-    document.write("")
+    alert("You must be a real hoot at a party.")
 }
-if(funAnswer == "OTHER"){
-    document.write("YOU HAVE WON THE PRIZE OF SCROLLING THROUGH MY ENTIRE PILE OF PINK!")
-}
+
 }
